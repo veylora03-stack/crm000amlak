21ba3f327a08d184b088958ac3d8feaface2ba5e
veylora03-stack <veylora03@gmail.com>
2026-08-08 23:29:58 +0330

    🐛 Fix: Remove duplicate export in api/index.js
    - Clean rewrite of api/index.js
    
    - Remove duplicate handleApiError export
    
    - Properly export all services and error handler
    
    - Fixes vite-plugin-pwa build error

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,12 +7,19 @@ export { createResource, createSubResource, createSingleton } from './resourceFa
 // Composables
 export { useApiResource, useSingletonResource } from '@/composables/useApiResource'
 
+// Error Handler
+export { handleApiError, parseApiError, withErrorHandling } from './errorHandler'
+
 // All service APIs
-export * from './services'
-// Error Handler
-export { handleApiError, parseApiError, withErrorHandling } from './errorHandler'
-
-
-// Error Handler
-export { handleApiError, parseApiError, withErrorHandling } from './errorHandler'
-
+export { default as authApi } from './services/auth'
+export { default as usersApi } from './services/users'
+export { default as clientsApi } from './services/clients'
+export { default as propertiesApi } from './services/properties'
+export { default as salesApi } from './services/sales'
+export { default as activitiesApi } from './services/activities'
+export { default as tasksApi } from './services/tasks'
+export { default as notificationsApi } from './services/notifications'
+export { default as dashboardApi } from './services/dashboard'
+export { default as reportsApi } from './services/reports'
+export { default as settingsApi } from './services/settings'
+export { default as searchApi } from './services/search'
